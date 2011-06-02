01807e45fd2f36ac6740a95001ae272ce2a5555c
Torben Jaeger <torben@jit-central.com>
2011-06-02 21:17:28 +0000

    added an optional filename argument for exporting

--- src/main/org/hornetq/core/persistence/tools/ExportData.java
+++ src/main/org/hornetq/core/persistence/tools/ExportData.java
@@ -13,33 +13,44 @@
 
 package org.hornetq.core.persistence.tools;
 
+import org.hornetq.core.journal.impl.SyncSpeedTest;
+
+import java.io.*;
+
 /**
  * A ExportData
  *
  * @author clebertsuconic
- *
- *
+ * @author <a href="mailto:torben@jit-consulting.de">Torben Jaeger</a>
  */
-public class ExportData
-{
+public class ExportData {
 
-   public static void main(String args[])
-   {
-      if (args.length != 2) {
+   public static void main(String args[]) {
+      if (args.length != 2 && args.length != 3) {
          // todo: maybe just use a hq-config.xml file as a parameter
-         System.out.println("Usage Export: java org.hornetq.core.persistence.tools.ExportData <bindings-dir> <journal-dir>");
+         System.out.println("Usage: java org.hornetq.core.persistence.tools.ExportData <bindings-dir> " +
+               "<journal-dir> [filename]");
+         System.out.println("[filename] is optional. If no filename is given, standard out is used!");
          System.exit(-1);
       }
-      try
-      {
-         ManageDataTool.exportMessages(args[0], args[1], System.out);
+
+      PrintStream output = System.out;
+      if (args.length == 3) {
+         try {
+            output = new PrintStream(new FileOutputStream(args[2],true));
+         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
+         }
       }
-      catch (Exception e)
-      {
+
+      try {
+         ManageDataTool.exportMessages(args[0], args[1], output);
+      } catch (Exception e) {
          e.printStackTrace();
          System.exit(-1);
       }
-      
+
    }
    // Constants -----------------------------------------------------
 
