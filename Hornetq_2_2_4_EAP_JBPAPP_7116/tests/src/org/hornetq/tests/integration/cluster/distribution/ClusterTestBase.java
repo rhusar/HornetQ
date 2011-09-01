fe2ba3841372a40095cf6acc27285934611a0ca7
Clebert Suconic <clebert.suconic@gmail.com>
2011-09-01 21:50:16 +0000

    back porting HORNETQ-753

--- Hornetq_2_2_4_EAP_JBPAPP_7116/tests/src/org/hornetq/tests/integration/cluster/distribution/ClusterTestBase.java
+++ Hornetq_2_2_4_EAP_JBPAPP_7116/tests/src/org/hornetq/tests/integration/cluster/distribution/ClusterTestBase.java
@@ -84,6 +84,16 @@ public abstract class ClusterTestBase extends ServiceTestBase
 
    private static final long WAIT_TIMEOUT = 5000;
 
+   protected int getLargeMessageSize()
+   {
+      return 1024;
+   }
+   
+   protected boolean isLargeMessage()
+   {
+      return false;
+   }
+   
    @Override
    protected void setUp() throws Exception
    {
@@ -522,6 +532,11 @@ public abstract class ClusterTestBase extends ServiceTestBase
          for (int i = msgStart; i < msgEnd; i++)
          {
             ClientMessage message = session.createMessage(durable);
+            
+            if (isLargeMessage())
+            {
+               message.setBodyInputStream(createFakeLargeStream(getLargeMessageSize()));
+            }
 
             if (filterVal != null)
             {
@@ -530,6 +545,7 @@ public abstract class ClusterTestBase extends ServiceTestBase
 
             message.putIntProperty(ClusterTestBase.COUNT_PROP, i);
 
+ 
             producer.send(message);
          }
       }
@@ -573,6 +589,11 @@ public abstract class ClusterTestBase extends ServiceTestBase
          for (int i = msgStart; i < msgEnd; i++)
          {
             ClientMessage message = session.createMessage(durable);
+            
+            if (isLargeMessage())
+            {
+               message.setBodyInputStream(createFakeLargeStream(getLargeMessageSize()));
+            }
 
             message.putStringProperty(key, val);
             message.putIntProperty(ClusterTestBase.COUNT_PROP, i);
@@ -766,6 +787,13 @@ public abstract class ClusterTestBase extends ServiceTestBase
                Assert.assertNotNull("consumer " + consumerID + " did not receive message " + j, message);
             }
 
+            if (isLargeMessage())
+            {
+               for (int posMsg = 0 ; posMsg < getLargeMessageSize(); posMsg++)
+               {
+                  assertEquals(getSamplebyte(posMsg), message.getBodyBuffer().readByte());
+               }
+            }
 
 
             if (ack)
